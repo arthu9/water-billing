aab4705fcf9369458b89a7e7600d26dcd60c8a9c
Joren Mundane <jorenpacaldo@yahoo.com>
2018-11-18 13:41:19 +0800

    Changed the HTML and Javascript Indention for each file

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
+import flask
+import requests
 from flask import Flask, jsonify, request, session, render_template, url_for, redirect
-import requests, json
-import flask, sys, os
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 app.secret_key = 'celeron0912'
+
+
 # app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/administrator')
 def admin_dash():
     if session.get('user') == None:
         return render_template('index.html')
     else:
-        return render_template("homepage_admin.html", firstname=str(session['firstname']), lastname=str(session['lastname']))
+        return render_template("homepage_admin.html", firstname=str(session['firstname']),
+                               lastname=str(session['lastname']))
+
 
 @app.route('/dashboard')
 def consumer_dash():
     if session.get('user') == None:
         return render_template('index.html')
     elif str(session['admin_prev']) == 'False':
-        return render_template("homepage_consumer.html", firstname=str(session['firstname']), lastname=str(session['lastname']), curuser=str(session['user']))
+        return render_template("homepage_consumer.html", firstname=str(session['firstname']),
+                               lastname=str(session['lastname']), curuser=str(session['user']))
     else:
         return redirect(url_for('admin_dash', firstname=str(session['firstname']), lastname=str(session['lastname'])))
+
 
 @app.route('/addbill')
 def admin_addbill():
     return render_template("addbill_admin.html", firstname=str(session['firstname']), lastname=str(session['lastname']))
+
 
 @app.route('/logout')
 def logout():
@@ -38,24 +46,27 @@ def logout():
     session.pop('admin_prev', None)
     return render_template('index.html')
 
+
 @app.route('/login', methods=['POST'])
 def login():
     params = request.get_json()
     username = params["username"]
     password = params["password"]
     headers = {'content-type': 'application/json; charset=utf-8', 'dataType': "json"}
-    resp = requests.post("http://localhost:8080/login", headers=headers, json={'username': username, 'password': password})
+    resp = requests.post("http://localhost:8080/login", headers=headers,
+                         json={'username': username, 'password': password})
     data = resp.json()
     if "error" in data['status']:
         return jsonify(resp.json())
     else:
-        resp2 = requests.get("http://localhost:8080/user/"+data['status'])
+        resp2 = requests.get("http://localhost:8080/user/" + data['status'])
         cur_user = resp2.json()
         session['firstname'] = cur_user['entries'][0]['firstname']
         session['lastname'] = cur_user['entries'][0]['lastname']
         session['admin_prev'] = cur_user['entries'][0]['admin_prev']
         session['user'] = data['status']
         return jsonify(resp.json())
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -69,10 +80,11 @@ def register():
     mobile = params["mobile"]
 
     headers = {'content-type': 'application/json; charset=utf-8', 'dataType': "json"}
-    resp = requests.post("http://localhost:8080/register", headers=headers, json={'firstname': firstname, 'lastname': lastname,
-                                                                               'username': rusername, 'password': password,
-                                                                               'address': address, 'reg_key': reg_key,
-                                                                               'number': mobile})
+    resp = requests.post("http://localhost:8080/register", headers=headers,
+                         json={'firstname': firstname, 'lastname': lastname,
+                               'username': rusername, 'password': password,
+                               'address': address, 'reg_key': reg_key,
+                               'number': mobile})
     data = resp.json()
     if "error" in data['status']:
         return jsonify(resp.json())
@@ -84,6 +96,7 @@ def register():
         session['admin_prev'] = cur_user['entries'][0]['admin_prev']
         session['user'] = data['user']
         return jsonify(resp.json())
+
 
 @app.after_request
 def add_cors(resp):
