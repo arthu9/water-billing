bec5896ea69bd0381f9cf2faad1f3f3cc36692c7
Joren Mundane <jorenpacaldo@yahoo.com>
2019-01-07 23:50:00 +0800

    rebase to master and finalized some of the UI

--- static/js/dashboard_admin.js
+++ static/js/dashboard_admin.js
@@ -4,6 +4,7 @@ let table1 = new Tabulator("#paid_table", {
     },
     height: 311,
     responsiveLayout: "hide",
+    selectable: false,
     layout: "fitColumns",
     columns: [
         {title: "Last name", field: "lastname", align: "center"},
@@ -19,6 +20,7 @@ let table2 = new Tabulator("#unpaid_table", {
         return response.entries;
     },
     height: 311,
+    selectable: false,
     responsiveLayout: "hide",
     layout: "fitColumns",
     columns: [
@@ -36,16 +38,17 @@ let table3 = new Tabulator("#disconnection_table", {
     },
     height: 311,
     responsiveLayout: "hide",
+    selectable: false,
     layout: "fitColumns",
     columns: [
         {title: "Last name", field: "lastname", align: "center"},
         {title: "First name", field: "firstname"},
-        {title: "Date Issued", field: "date_of_bill", sorter:"date"},
-        {title: "Reading", field: "reading"},
-        {title: "Amount", field: "amount"}
+        {title: "Address", field: "address"},
+        {title: "Mobile Number", field: "mobile"},
+        {title: "# of Unpaid Bills", field: "unpaid_count"}
     ]
 });
 
 table1.setData('http://localhost:8080/viewpaid/Paid');
 table2.setData('http://localhost:8080/viewpaid/Unpaid');
-table3.setData('http://localhost:8080/viewpaid/Disconnected');
+table3.setData('http://localhost:8080/bill/disconnection');
